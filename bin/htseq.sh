8b95c30313baf42b3807ffeb6e219f07eac72809
aymanabuelela <ayman.elkhodiery@kaust.edu.sa>
2015-05-06 11:32:41 +0300

    add script arguments

--- bin/htseq.sh
+++ bin/htseq.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
-###############################################################################
-###############################################################################
-##           this script takes 2 arguments:                                  ##
-##           1. Number of processors (int)                                   ##
-##           2. Memory allocated per processor (int)in Gbytes                ##
-###############################################################################
-###############################################################################
-
-
 np=$1
 cnt=1
-tophatdirs="/media/khodieaf/Data/GBM/tophat/out.dirs"
-refgenome="/ake/GT_paper/DEG/rna-seq/RefGenomeUCSC/Homo_sapiens/UCSC/hg19/Annotation/Genes/genes.gtf"
-bamdir="/media/khodieaf/Data/GBM/tophat"
-sortedbam="/media/khodieaf/Data/GBM/sorted_bam"
-outdir="/media/khodieaf/Data/GBM/rawcounts"
 
-cd $bamdir
+if [ $# -lt 7 ]; then
+    echo "ERROR: one or more arguments are missing!!
 
-for i in SRR*
-    do
+This script takes 7 arguments:
+        1. Number of processors                     (int)
+        2. Memory allocated per processor in Gb     (int)
+        3. tophat output folders to be processed    (text file)
+        4. tophat folder                            (full path)
+        5. sorted bam folder                        (full path)
+        6. reference genome gtf file                (full path)
+        7. rawcounts folder                         (full path)"
+    exit 0
+fi
+
+htseqFun () 
+{       
+    samtools sort -m $2"G" $4/$i/"accepted_hits.bam" $5/$i".sorted" \
+    && htseq-count -m union -t exon -i gene_id -f bam -r pos -s yes \
+        $5/$i".sorted.bam" $6 > $7/$i".txt" \
+    && rm $5/$i".sorted.bam" &
+}
+
+while read i; do
     echo "[$i] `date`"
 
-    if [ $cnt -lt $np ]
-        then
+    if [ $cnt -lt $np ]; then
+        htseqFun
         cnt=$(($cnt+1))
-        samtools sort -m $2"G" "$bamdir/$i/accepted_hits.bam" "$sortedbam/$i.sorted" \
-        && htseq-count -m union -t exon -i gene_id -f bam -r pos -s yes "$sortedbam/$i.sorted.bam" $refgenome > "$outdir/$i.txt" &
     else
-        samtools sort -m $2"G" "$bamdir/$i/accepted_hits.bam" "$sortedbam/$i.sorted" \
-        && htseq-count -m union -t exon -i gene_id -f bam -r pos -s yes "$sortedbam/$i.sorted.bam" $refgenome > "$outdir/$i.txt" &
+        htseqFun
         wait
-        $cnt=1
+        cnt=1
     fi
-done 
+done < $3
